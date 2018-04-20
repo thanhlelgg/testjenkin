70d0340efb0cdb08e52e59b00ed14cff06868c59
Thanh Le <thanh.thuan.le@logigear.com>
2018-04-20 11:37:00 +0700

    testing

--- test/sendmail.py
+++ test/sendmail.py
@@ -3,6 +3,6 @@ file = open("testfile.txt","w")
 file.write("Hello Worlds")
 file.write("This is our new text file")
 file.write("and this is another line.")
-file.write("Why? Because we can.")
+file.write("Why? Because we can.11111")
 
 file.close()
